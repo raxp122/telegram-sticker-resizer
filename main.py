b34092c7695263bb89814c64cd9d76659b99b800
raxp122 <68340186+raxp122@users.noreply.github.com>
2021-01-14 14:09:11 +0100

    Added main.py
    Resize images keeping aspect ratio with max width or height set to 512 to meet the Telegram @Stickers standard. Max size can be changed to 100px to make icons for Sticker Packs.

---
+++ main.py
@@ -0,0 +1,46 @@
+from PIL import Image
+import os
+from os import listdir
+from os.path import isfile, join
+from pathlib import Path
+
+# generate directories
+try:
+    os.mkdir("Input")
+except OSError:
+    print("Failed directories creation or directories already exists")
+else:
+    print("Directories created")
+try:
+    os.mkdir("Output")
+except OSError:
+    print("Failed directories creation or directories already exists")
+else:
+    print("Directories created")
+
+# get names of files in Input folder
+onlyfiles = [f for f in listdir("Input") if isfile(join("Input", f))]
+
+# set the size of the image for telegram stickers
+max_side = 512
+
+
+# resize every file in Input folder and save in Output folder
+for file in onlyfiles:
+    # open file
+    img = Image.open('Input/' + file)
+    if img.size[0] >= img.size[1]:
+        width = max_side
+        height = int((max_side / img.size[0]) * img.size[1])
+        size = width, height
+    elif img.size[1] > img.size[0]:
+        width = int((max_side / img.size[1]) * img.size[0])
+        height = max_side
+        size = width, height
+    # get name without extension
+    name = Path(file).stem
+    # resize the image
+    img = img.resize(size, Image.NEAREST)
+    # save resized image
+    img.save("Output/" + name + ".png", "PNG")
+
